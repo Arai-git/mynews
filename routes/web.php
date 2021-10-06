04b74c78b9a098a3f564baa33394e1d040f814d4
S8T <10l.mah.s11@gmail.com>
2021-10-06 02:30:11 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');
 });
 
 Route::get('XXX', 'AAAController@BBB');
@@ -25,10 +29,14 @@ Route::get('XXX', 'AAAController@BBB');
 Route::group(['prefix' => 'admin'], function(){
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
 });
 
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('profile/create','Admin\ProfileController@add');
+     Route::get('profile/edit','Admin\ProfileController@edit');
+     Route::post('profile/create', 'Admin\ProfileController@create');
+     Route::post('profile/edit', 'Admin\ProfileController@update');
+});
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
