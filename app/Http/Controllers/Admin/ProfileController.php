6a7b7483302e213bf7edf9fec70b81a5e4e5b074
S8T <10l.mah.s11@gmail.com>
2021-10-06 09:01:10 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,16 +5,38 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
-    //
+    
     public function add()
     {
         return view('admin.profile.create');
     }
-
-    public function create()
+    
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        
+      $news = new Profile;
+      $form = $request->all();
+      
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+      
+      unset($form['_token']);
+      
+      unset($form['image']);
+      
+      $news->fill($form);
+      $news->save();
+      
         return redirect('admin/profile/create');
     }
 
