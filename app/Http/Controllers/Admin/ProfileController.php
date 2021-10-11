de9e11180369a3f30696871bb204645b87da3e28
S8T <10l.mah.s11@gmail.com>
2021-10-11 06:33:46 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,37 +17,66 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
-        $this->validate($request, Profile::$rules);
+      $this->validate($request, Profile::$rules);
         
-      $news = new Profile;
+      $profile = new Profile();
       $form = $request->all();
-      
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-      if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
-      } else {
-          $news->image_path = null;
-      }
+    
+    
       
       unset($form['_token']);
       
       unset($form['image']);
-      
-      $news->fill($form);
-      $news->save();
+      $profile->fill($form);
+      $profile->save();
       
         return redirect('admin/profile/create');
     }
 
-    public function edit()
+    
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        $cond_title = $request->cond_title;
+        if($cond_title!=''){
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+        }
+        
+        
+    public function edit(Request $request)
+    {
+        $profile = Profile::find($request->id);
+        
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    
+    
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile/');
+    }
+    
+    
+    public function delete(Request $request)
+    {
+        $profile = Profile::find($request->id);
+        
+        $profile->delete();
+        return redirect('admin/profile/');
     }
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+
+
 }
 
