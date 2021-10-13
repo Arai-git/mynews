ddcd643d78d8ccbe42661923279dc10a4e8a16d1
S8T <10l.mah.s11@gmail.com>
2021-10-13 09:14:49 +0000

    heroku deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environemnt('production')) {
+        if (\App::environment('production')) {
             \URL::forceSchema('https');
         }
     }
